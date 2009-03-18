d16f739add6e21eb8779a6cc783840b15923ee12
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-03-18 01:40:37 +0000

    Added second Firewire stdArrays plugin
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@8644 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocFirewire/st.cmd
+++ iocBoot/iocFirewire/st.cmd
@@ -9,7 +9,9 @@ firewireWinDCAMApp_registerRecordDeviceDriver(pdbbase)
 # This is the Thorlabs camera
 #WinFDC_Config("FW1", "116442682213159680", 50, 200000000)
 # This is the SONY camera
-WinFDC_Config("FW1", "163818473825504512", 50, 200000000)
+#WinFDC_Config("FW1", "163818473825504512", 50, 200000000)
+# This will use the first camera found without needing to know its ID
+WinFDC_Config("FW1", "", 50, 200000000)
 asynSetTraceIOMask("FW1",0,2)
 #asynSetTraceMask("FW1",0,255)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=13FW1:,R=cam1:,PORT=FW1,ADDR=0,TIMEOUT=1")
@@ -17,15 +19,18 @@ dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=13FW1:,R=cam1:,P
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/firewireDCAM.template",   "P=13FW1:,R=cam1:,PORT=FW1,ADDR=0,TIMEOUT=1")
 dbLoadTemplate("firewire.substitutions")
 
-#prosilicaConfig("PS2", 50022, 10, 50000000)
-#dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=13FW1:,R=cam2:,PORT=PS2,ADDR=0,TIMEOUT=1")
-#dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=13FW1:,R=cam2:,PORT=PS2,ADDR=0,TIMEOUT=1")
-
-# Create a standard arrays plugin, set it to get data from first Prosilica driver.
+# Create a standard arrays plugin, set it to get 8-bit data from the driver.
 drvNDStdArraysConfigure("FW1Image", 5, 0, "FW1", 0, -1)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13FW1:,R=image1:,PORT=FW1Image,ADDR=0,TIMEOUT=1,NDARRAY_PORT=FW1,NDARRAY_ADDR=0")
 # This is enough elements for 1376*1024*3
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=13FW1:,R=image1:,PORT=FW1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=4227072")
+
+# Create a second standard arrays plugin, set it to get 16-bit data from the driver.
+drvNDStdArraysConfigure("FW1Image2", 5, 0, "FW1", 0, -1)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13FW1:,R=image2:,PORT=FW1Image2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=FW1,NDARRAY_ADDR=0")
+# This is enough elements for 1376*1024*3
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=13FW1:,R=image2:,PORT=FW1Image2,ADDR=0,TIMEOUT=1,SIZE=16,FTVL=SHORT,NELEMENTS=4227072")
+
 # Load the database to use with Stephen Mudie's IDL code
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/EPICS_AD_Viewer.template", "P=13FW1:, R=image1:")
 
@@ -43,14 +48,10 @@ dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROIN.template",      "P=13FW1:,R=ROI1
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROIN.template",      "P=13FW1:,R=ROI1:2:,PORT=FW1ROI,ADDR=2,TIMEOUT=1,HIST_SIZE=256")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROIN.template",      "P=13FW1:,R=ROI1:3:,PORT=FW1ROI,ADDR=3,TIMEOUT=1,HIST_SIZE=256")
 
-# Create 2 color conversion plugins
+# Create a color conversion plugin
 drvNDColorConvertConfigure("FW1CC1", 5, 0, "FW1", 0, 20, -1)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","   P=13FW1:,R=CC1:,  PORT=FW1CC1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=FW1,NDARRAY_ADDR=0")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDColorConvert.template", "P=13FW1:,R=CC1:,  PORT=FW1CC1,ADDR=0,TIMEOUT=1")
-drvNDColorConvertConfigure("FW1CC2", 5, 0, "FW1", 0, 20, -1)
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","   P=13FW1:,R=CC2:,  PORT=FW1CC2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=FW1CC1,NDARRAY_ADDR=0")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDColorConvert.template", "P=13FW1:,R=CC2:,  PORT=FW1CC2,ADDR=0,TIMEOUT=1")
-
 
 #asynSetTraceMask("FW1",0,255)
 
